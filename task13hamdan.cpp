3e95e40b48793d54e08c92b021143fc5a036dd2f
Hamdanaleem <144408426+Hamdanaleem@users.noreply.github.com>
2023-09-24 05:21:19 -0700

    Add files via upload

---
+++ task13hamdan.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num;
+	int sum=0;
+	cout<<"enter num1: ";
+	cin >>num;
+	sum=sum+num;
+	cout<<"enter num2: ";
+	cin >>num;
+	sum=sum+num;
+	cout<<"enter num3: ";
+	cin >>num;
+	sum=sum+num;
+	cout<<"enter num4: ";
+	cin >>num;
+	sum=sum+num;
+	cout<<"enter num5: ";
+	cin >>num;
+	sum=sum+num;
+	cout<<"result: "<<sum;
+}
